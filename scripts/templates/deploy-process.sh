2c0602b1f2a03e02fddd450f47530b19f3d697e6
bnicolae <bnicolae@43460812-3f57-0410-813e-9589cf9fb6d1>
2009-01-20 09:06:16 +0000

    moved the G5K scripts outside of main trunk
    git-svn-id: svn://scm.gforge.inria.fr/svn/blobseer/trunk@35 43460812-3f57-0410-813e-9589cf9fb6d1

--- scripts/templates/deploy-process.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#create directories and launch command: $1=path to command, $2=command, $3=config file, $4=running dir
-rm -r -f $4
-mkdir -p $4
-$1/$2 $3 >$4/$2.stdout 2>$4/$2.stderr &
