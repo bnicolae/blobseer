68a45a1ce128f21d4f36956f94e1f8a5f5e42e7b
bnicolae <bnicolae@43460812-3f57-0410-813e-9589cf9fb6d1>
2009-02-17 10:04:08 +0000

    page manager now supports listeners; elliminated sdht duplicate this way
    git-svn-id: svn://scm.gforge.inria.fr/svn/blobseer/trunk@48 43460812-3f57-0410-813e-9589cf9fb6d1

---
+++ provider/pmgr_listener.cpp
@@ -0,0 +1,65 @@
+#include "pmgr_listener.hpp"
+#include "common/debug.hpp"
+
+pmgr_listener::pmgr_listener(boost::asio::io_service &io_service,
+		     const provider_adv &a, 
+		     const std::string &phost, 
+		     const std::string &pservice,
+		     const unsigned int rc) : 
+    publisher_host(phost), publisher_service(pservice), adv(a), retry_count(rc) {
+
+    publisher = new rpc_client_t(io_service);
+    timeout_timer = new boost::asio::deadline_timer(io_service);
+    update_rate = adv.get_update_rate();
+    update(~(unsigned int)0);
+}
+
+pmgr_listener::~pmgr_listener() {    
+    delete publisher;
+    delete timeout_timer;
+}
+
+void pmgr_listener::update_event(const boost::int32_t name, const page_manager::monitored_params_t &params) {
+    switch (name) {
+    case PROVIDER_WRITE:
+	adv.set_free(params.get<0>());
+	update(retry_count);
+	break;
+    case PROVIDER_READ:
+	break;
+    default:
+	ERROR("Unknown hook type: " << name);
+    }
+}
+
+void pmgr_listener::update(unsigned int retry_count) {
+    if (update_rate == adv.get_update_rate()) {
+	update_rate = 0;
+	rpcvector_t params;
+	params.push_back(buffer_wrapper(adv, true));
+	publisher->dispatch(publisher_host, publisher_service, PUBLISHER_UPDATE, params, 
+			    boost::bind(&pmgr_listener::provider_callback, this, retry_count, _1, _2));
+    } else
+	update_rate++;
+}
+
+void pmgr_listener::timeout_callback(unsigned int retry_count,
+				    const boost::system::error_code& error) {
+    if (error != boost::asio::error::operation_aborted) {
+	rpcvector_t params;
+	params.push_back(buffer_wrapper(adv, true));
+	publisher->dispatch(publisher_host, publisher_service, PUBLISHER_UPDATE, params, 
+			    boost::bind(&pmgr_listener::provider_callback, this, retry_count - 1, _1, _2));
+    }
+}
+
+void pmgr_listener::provider_callback(unsigned int retry_count,
+				     const rpcreturn_t &error, const rpcvector_t &/*buff_result*/) {
+    if ((retry_count > 0) && error != rpcstatus::ok) {
+	INFO("update callback failure, retrying in " << RETRY_TIMEOUT << "s (" << retry_count << " retries left)");
+	timeout_timer->expires_from_now(boost::posix_time::seconds(RETRY_TIMEOUT));
+	timeout_timer->async_wait(boost::bind(&pmgr_listener::timeout_callback, this, retry_count, _1));	
+    } else {
+	INFO("update callback success");
+    }
+}
---
+++ provider/pmgr_listener.hpp
@@ -0,0 +1,38 @@
+#ifndef __PMGR_LISTENER
+#define __PMGR_LISTENER
+
+#include <vector>
+
+#include "common/config.hpp"
+#include "rpc/rpc_client.hpp"
+#include "provider/provider_adv.hpp"
+#include "pmanager/publisher.hpp"
+#include "provider/provider.hpp"
+#include "provider/page_manager.hpp"
+
+class pmgr_listener {
+    typedef rpc_client<config::socket_namespace, config::lock_t> rpc_client_t;
+
+    static const unsigned int RETRY_TIMEOUT = 10;
+
+    std::string publisher_host, publisher_service;
+    provider_adv adv;
+    unsigned int update_rate, retry_count;
+    rpc_client_t *publisher;
+    boost::asio::deadline_timer *timeout_timer;
+
+    void update(unsigned int retry_count);
+    void provider_callback(unsigned int retry_count, const rpcreturn_t &error, const rpcvector_t &answer);
+    void timeout_callback(unsigned int retry_count, const boost::system::error_code& error);
+
+public:
+    void update_event(const boost::int32_t name, const page_manager::monitored_params_t &params);
+    pmgr_listener(boost::asio::io_service &io_service,
+	      const provider_adv &adv, 
+	      const std::string &phost, 
+	      const std::string &pservice,
+	      const unsigned int retry_count);
+    ~pmgr_listener();
+};
+
+#endif
