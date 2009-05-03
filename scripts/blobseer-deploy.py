beb9b9a0ac61503fd8c20d0deae7def9ead22181
bnicolae <bnicolae@43460812-3f57-0410-813e-9589cf9fb6d1>
2009-05-03 17:14:46 +0000

    more flexible deployment script
    git-svn-id: svn://scm.gforge.inria.fr/svn/blobseer/trunk@84 43460812-3f57-0410-813e-9589cf9fb6d1

--- scripts/blobseer-deploy.py
+++ scripts/blobseer-deploy.py
@@ -49,18 +49,24 @@ def gen_kill(path, process):
 def gen_status(path, process):
     return "\"test `ps aux | grep " + path + "/" + process + " | grep -v grep | wc -l` -eq 1 || echo no " + process + " running\""
 
-class CommandLauncher(threading.Thread):
+class RemoteCopy(threading.Thread):
+    def __init__(self, n, src, dest):
+        threading.Thread.__init__(self)
+        self.node = n
+        self.cmd = SCP + " " + src + " " + self.node + ":" + dest + " &>/dev/null"
+
+    def run(self):
+        p = subprocess.Popen(self.cmd, shell=True)
+        if p.wait() != 0:
+            print "[%s]: could not copy configuration file" % self.node
+
+class RemoteLauncher(threading.Thread):
     def __init__(self, n, cmd):
         threading.Thread.__init__(self)
         self.node = n
         self.cmd = SSH + " " + self.node + " " + cmd
             
     def run(self):
-        if (needcfg):
-            p = subprocess.Popen(SCP + " " + TMP_CFG_FILE + " " + self.node + ":" + REMOTE_CFG_FILE + " &>/dev/null", shell=True)
-            if p.wait() != 0:
-                print "[%s]: could not copy configuration file" % self.node
-                return
         p = subprocess.Popen(self.cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (cout, cerr) = p.communicate()
         if cout.strip() != "":
@@ -75,7 +81,6 @@ pmgr = ""
 providers = []
 dht = []
 gencmd = gen_launch
-needcfg = False
 for (o, a) in opts:
     if o in ("-h", "--help"):
         usage()
@@ -91,8 +96,7 @@ for (o, a) in opts:
     elif o in ("-l", "--launch"):
         f = open(a, "r")
         template = f.readlines()
-        f.close()
-        needcfg = True
+        f.close()       
     elif o in ("-k", "--kill"):
         gencmd = gen_kill
     elif o in ("-s", "--status"):
@@ -102,32 +106,45 @@ if vmgr == "" or pmgr == "" or providers == [] or dht == []:
     usage()
     sys.exit(1)
 
-if (needcfg):
+workers = []
+
+# If needed, copy all configuration files to the remote nodes
+if (gencmd == gen_launch):
     gateways = ",\n\t\t".join(map(lambda x: "\"" + x + "\"", dht))
     f = open(TMP_CFG_FILE, "w")
     f.writelines(map(lambda x: x.replace("${gateways}", gateways).
                      replace("${vmanager}", "\"" + vmgr + "\"").
                      replace("${pmanager}", "\"" + pmgr + "\""), template))
     f.close()
-
-workers = []
-workers.append(CommandLauncher(vmgr, gencmd("vmanager", "vmanager")))
-workers.append(CommandLauncher(pmgr, gencmd("pmanager", "pmanager")))
-for p in providers:
-    workers.append(CommandLauncher(pmgr, gencmd("provider", "provider")))
-for d in dht:
-    workers.append(CommandLauncher(pmgr, gencmd("provider", "sdht")))
     
-# Now launching workers
+    cfgs = set([vmgr]) | set([pmgr]) | set(providers) | set(dht)
+    for x in cfgs:
+        workers.append(RemoteCopy(x, TMP_CFG_FILE, REMOTE_CFG_FILE))
+
+    for w in workers:
+        w.start()
+
+    for w in workers:
+        w.join()
+    
+workers = []
+workers.append(RemoteLauncher(vmgr, gencmd("vmanager", "vmanager")))
+workers.append(RemoteLauncher(pmgr, gencmd("pmanager", "pmanager")))
+for p in providers:
+    workers.append(RemoteLauncher(p, gencmd("provider", "provider")))
+for d in dht:
+    workers.append(RemoteLauncher(d, gencmd("provider", "sdht")))
+    
 for w in workers:
     w.start()
 
-# Wait for all workers to finish
 for w in workers:
     w.join()
 
 # Do some cleanup
-if (needcfg):
+try:
     os.remove(TMP_CFG_FILE)
+except OSError:
+    pass
 
 print "All done!"
