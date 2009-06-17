6f822300a58d3854c13c159c3fb657b25e7bfe1f
bnicolae <bnicolae@43460812-3f57-0410-813e-9589cf9fb6d1>
2009-06-17 15:54:04 +0000

    cleaned up bindings
    git-svn-id: svn://scm.gforge.inria.fr/svn/blobseer/trunk@106 43460812-3f57-0410-813e-9589cf9fb6d1

--- bindings/java/blobseer_ObjectHandler.cpp
+++ bindings/java/blobseer_ObjectHandler.cpp
@@ -30,50 +30,53 @@ JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_blob_1free(JNIEnv *env, j
 }
 
 JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_blob_1create(JNIEnv *env, jclass c, jlong blob, jlong ps, jint rc) {
-	return ((object_handler*)blob)->create(ps,rc);
+    return ((object_handler*)blob)->create(ps,rc);
 }
 
-JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_get_1latest(JNIEnv *env, jclass c, jlong blob, jint blob_id) {
-	return ((object_handler*)blob)->get_latest(blob_id);
+JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_get_1latest(JNIEnv *env, jclass c, jlong blob, jint blob_id ) {
+    return ((object_handler*)blob)->get_latest(blob_id);
 }
 
 JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_read(JNIEnv *env, jclass c, jlong blob, jlong offset, jlong size, jbyteArray jbuffer, jint version) {
-	char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
-	bool result = ((object_handler*)blob)->read(offset,size,buffer,version);
-	env->ReleaseByteArrayElements(jbuffer,(jbyte*)buffer,0);
-	return result;
+    char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
+    bool result = ((object_handler*)blob)->read(offset, size, buffer, version);
+    env->ReleaseByteArrayElements(jbuffer, (jbyte*)buffer, 0);
+
+    return result;
 }
 
 JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_append(JNIEnv *env, jclass c, jlong blob, jlong size, jbyteArray jbuffer) {
-	char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
-	bool result = ((object_handler*)blob)->append(size,buffer);
-	env->ReleaseByteArrayElements(jbuffer,(jbyte*)buffer,0);
-	return result;
+    char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
+    bool result = ((object_handler*)blob)->append(size, buffer);
+    env->ReleaseByteArrayElements(jbuffer, (jbyte*)buffer, 0);
+
+    return result;
 }
 
 JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_write(JNIEnv *env, jclass c, jlong blob, jlong offset, jlong size, jbyteArray jbuffer) {
-	char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
-	bool result = ((object_handler*)blob)->write(offset,size,buffer);
-	env->ReleaseByteArrayElements(jbuffer,(jbyte*)buffer,0);
-	return result;
+    char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
+    bool result = ((object_handler*)blob)->write(offset, size, buffer);
+    env->ReleaseByteArrayElements(jbuffer, (jbyte*)buffer, 0);
+
+    return result;
 }
 
 JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1objcount(JNIEnv *env, jclass c, jlong blob) {
-	return ((object_handler*)blob)->get_objcount();
+    return ((object_handler*)blob)->get_objcount();
 }
 
 JNIEXPORT jlong JNICALL Java_blobseer_ObjectHandler_get_1size(JNIEnv *env, jclass c, jlong blob, jint version) {
-	return ((object_handler*)blob)->get_size(version);
+    return ((object_handler*)blob)->get_size(version);
 }
 
 JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1version(JNIEnv *env, jclass c, jlong blob) {
-	return ((object_handler*)blob)->get_version();
+    return ((object_handler*)blob)->get_version();
 }
 
 JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1page_1size(JNIEnv *env, jclass c, jlong blob) {
-	return ((object_handler*)blob)->get_page_size();
+    return ((object_handler*)blob)->get_page_size();
 }
 
 JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1id(JNIEnv *env, jclass c, jlong blob) {
-	return ((object_handler*)blob)->get_id();
+    return ((object_handler*)blob)->get_id();
 }
