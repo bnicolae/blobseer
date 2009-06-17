c9d61e8d2c946fddc7d959c8c1d0ea93918f5a1f
mdorier <mdorier@43460812-3f57-0410-813e-9589cf9fb6d1>
2009-06-17 14:19:29 +0000

    Java binding now working. See java/blobseer/ObjectHandler.java for the object handler class, and java/Test.java to have an example of use.
    git-svn-id: svn+ssh://bnicolae@scm.gforge.inria.fr/svn/blobseer/trunk@102 43460812-3f57-0410-813e-9589cf9fb6d1

---
+++ java/Test.java
@@ -0,0 +1,56 @@
+import blobseer.ObjectHandler;
+import java.io.IOException;
+/* ========================== 
+ Test program for the java binding
+ created by Matthieu Dorier
+ date : 5.17.2009
+ =========================== */
+class Test {
+	
+    public static void main(String[] args) {
+	    
+		ObjectHandler handler;
+		byte[] buffer = new byte[1024];
+		
+		if(args.length != 1)
+		{
+			System.out.println("Usage : java Test <configfile>");
+			return;
+		}
+		
+		try {
+			handler = new ObjectHandler(args[0]);
+		}catch(IOException e){
+			System.err.println("IOException catched.");
+			return;
+		}
+		
+		// trying to create a blob
+		handler.blobCreate(1024, 1);
+		
+		// trying to append 2 pages of memory
+		handler.append(2048);
+		
+		// trying to write something
+		for(int i=0;i<1024;i++) buffer[i] = (byte)i;
+		handler.write(0, 1024, buffer);
+		
+		// trying to re-read
+		byte[] result = new byte[1024];
+		for(int i=0;i<1024;i++) result[i] = (byte)0;
+		handler.getLatest(handler.getId());
+		handler.read(0,1024,result,0);
+		
+		// compare the result with what we have written
+		boolean c = true;
+		for(int i=0;i<1024;i++) c = c & (result[i] == (byte)(i%256));
+		System.out.println(c);
+		
+		// other tests
+		System.out.println(handler.getObjCount());
+		System.out.println(handler.getSize());
+		System.out.println(handler.getPageSize());
+		System.out.println(handler.getId());
+
+    }
+}
---
+++ java/blobseer_ObjectHandler.cpp
@@ -0,0 +1,79 @@
+#include "blobseer_ObjectHandler.h"
+#include "client/object_handler.hpp"
+
+#include "common/debug.hpp"
+
+void __attribute__ ((constructor)) blobseer_init() {
+}
+void __attribute__ ((destructor)) blobseer_fini() {
+}
+
+JNIEXPORT jlong JNICALL Java_blobseer_ObjectHandler_blob_1init(JNIEnv *env, jclass c, jstring jcfg) {
+    jlong ret = 0;
+    const char *cfg = env->GetStringUTFChars(jcfg, NULL);
+    std::string cfg_file(cfg);
+    env->ReleaseStringUTFChars(jcfg, cfg);
+    try {
+	object_handler *h = new object_handler(cfg_file);
+	ret = (jlong)h;
+    } catch (std::runtime_error &e) {
+	ERROR(e.what());
+    }
+    return ret;
+}
+
+JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_blob_1free(JNIEnv *env, jclass c, jlong blob) {
+    if (blob == 0)
+	return false;
+    delete (object_handler *)blob;
+    return true;
+}
+
+JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_blob_1create(JNIEnv *env, jclass c, jlong blob, jlong ps, jint rc) {
+	return ((object_handler*)blob)->create(ps,rc);
+}
+
+JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_get_1latest(JNIEnv *env, jclass c, jlong blob, jint blob_id) {
+	return ((object_handler*)blob)->get_latest(blob_id);
+}
+
+JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_read(JNIEnv *env, jclass c, jlong blob, jlong offset, jlong size, jbyteArray jbuffer, jint version) {
+	char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
+	bool result = ((object_handler*)blob)->read(offset,size,buffer,version);
+	env->ReleaseByteArrayElements(jbuffer,(jbyte*)buffer,0);
+	return result;
+}
+
+JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_append(JNIEnv *env, jclass c, jlong blob, jlong size, jbyteArray jbuffer) {
+	char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
+	bool result = ((object_handler*)blob)->append(size,buffer);
+	env->ReleaseByteArrayElements(jbuffer,(jbyte*)buffer,0);
+	return result;
+}
+
+JNIEXPORT jboolean JNICALL Java_blobseer_ObjectHandler_write(JNIEnv *env, jclass c, jlong blob, jlong offset, jlong size, jbyteArray jbuffer) {
+	char *buffer = (char*)(env->GetByteArrayElements(jbuffer, 0));
+	bool result = ((object_handler*)blob)->write(offset,size,buffer);
+	env->ReleaseByteArrayElements(jbuffer,(jbyte*)buffer,0);
+	return result;
+}
+
+JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1objcount(JNIEnv *env, jclass c, jlong blob) {
+	return ((object_handler*)blob)->get_objcount();
+}
+
+JNIEXPORT jlong JNICALL Java_blobseer_ObjectHandler_get_1size(JNIEnv *env, jclass c, jlong blob, jint version) {
+	return ((object_handler*)blob)->get_size(version);
+}
+
+JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1version(JNIEnv *env, jclass c, jlong blob) {
+	return ((object_handler*)blob)->get_version();
+}
+
+JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1page_1size(JNIEnv *env, jclass c, jlong blob) {
+	return ((object_handler*)blob)->get_page_size();
+}
+
+JNIEXPORT jint JNICALL Java_blobseer_ObjectHandler_get_1id(JNIEnv *env, jclass c, jlong blob) {
+	return ((object_handler*)blob)->get_id();
+}
