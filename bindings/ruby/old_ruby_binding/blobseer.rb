d7839c6f27fddf7d38ce29cda2436443ce678de0
mdorier <mdorier@43460812-3f57-0410-813e-9589cf9fb6d1>
2009-08-26 13:30:18 +0000

    new ruby binding using the C++ object_handler. WARNING : may need to be more tested.
    git-svn-id: svn://scm.gforge.inria.fr/svn/blobseer/trunk@173 43460812-3f57-0410-813e-9589cf9fb6d1

--- bindings/ruby/old_ruby_binding/blobseer.rb
+++ bindings/ruby/old_ruby_binding/blobseer.rb
@@ -3,7 +3,7 @@ require 'libblobseer_ruby'
 # BlobSeer: Ruby binding
 # by Matthieu Dorier (matthieu.dorier@irisa.fr)
 #      blobseer version : 0.3.1
-#      ruby binding version: 0.5       date: 08.26.2009
+#      ruby binding version: 0.4       date: 6.17.2009
 #============================================================#
 class ObjectHandler
 
@@ -12,10 +12,12 @@ class ObjectHandler
 	# file. Raise an error if the config file doesn't 
 	# exists.
 	def initialize(config)
+		@blob_entry = BLOBSEER::Blob_t.new
 		if not FileTest.exists?(config)
 			raise "#{config} : file not found.\n"
 		end
-		@cpp_object_handler = BLOBSEER::CppObjectHandler.new(config)
+		@environment = BLOBSEER._init_(config)
+		@initialize = false
 	end
 
 	#----------------------------------------------------#
@@ -26,66 +28,113 @@ class ObjectHandler
 		if page_size <= 0
 			raise "page size must be > 0."
 		end
-		return BLOBSEER._create_(@cpp_object_handler, page_size, replica_count)
+		@blob_entry =  BLOBSEER._create_(@environment, page_size, replica_count)
+		if @blob_entry == nil
+			raise "while creating a blob."
+		end
+		@initialized = true
 	end
 	
 	#----------------------------------------------------#
 	# get the id of the current blob.
 	def get_id
-		return BLOBSEER._get_id_(@cpp_object_handler)
+		if @initialized
+			return BLOBSEER._getid_(@blob_entry)
+		else
+			return 0
+		end
 	end
 	
 	#----------------------------------------------------#
 	# get the version of the current blob.
 	def get_version
-		return BLOBSEER._get_version_(@cpp_object_handler)
+		if @initialized
+			return BLOBSEER._getversion_(@blob_entry)
+		else
+			return 0
+		end
 	end
 
 	#----------------------------------------------------#
 	# read into the blob, given offset and size. If
 	# version is ommited, default is the latest one.
 	def read(offset,size,version=-1)
-		if version == -1
-			version = BLOBSEER._get_version_(@cpp_object_handler)
+		if @initialized
+			if version == -1
+				version = BLOBSEER._getversion_(@blob_entry)
+			end
+			return BLOBSEER._read_(@blob_entry,version,offset,size)
+		else
+			raise "blob must be initialized."
 		end
-		return BLOBSEER._read_(@cpp_object_handler,offset,size,version)
 	end
 	
 	#----------------------------------------------------#
 	# write a string into the blob.
 	def write(offset,str)
-		return BLOBSEER._write_(@cpp_object_handler,offset,str.size,str)
+		if @initialized
+			if BLOBSEER._write_(@blob_entry,offset,str.size,str)
+				return true
+			else
+				return false
+			end
+		else
+			raise "blob must be initialized."
+		end
 	end
 
 	#----------------------------------------------------#
 	# get the latest status of a given blob, using its
 	# blob id.
 	def get_latest(blob_id)
-		return BLOBSEER._get_latest_(@cpp_object_handler, blob_id)
+		@blob_entry = BLOBSEER._setid_(@environment, blob_id,@blob_entry)
+		BLOBSEER._getlatest_(@blob_entry)
+		if @blob_entry == nil
+			raise "while connecting to the blob."
+		end
+		@initialized = true
+		return blob_id
 	end
 	
 	#----------------------------------------------------#
 	# get the current page size.
 	def get_page_size
-		return BLOBSEER._get_page_size_(@cpp_object_handler)
+		if @initialized
+			return BLOBSEER._getpagesize_(@blob_entry)
+		else
+			return 0
+		end
 	end
 
 	#----------------------------------------------------#
 	# get the current size.
 	def get_size(version = -1)
-		if version == -1
-			version = BLOBSEER._get_version_(@cpp_object_handler)
+		if @initialized
+			if version != -1
+				return BLOBSEER._getsize_(@blob_entry, version)
+			else
+				return BLOBSEER._getcurrentsize_(@blob_entry)
+			end
+		else
+			raise "blob must be initialized."
 		end
-		return BLOBSEER._getsize_(@cpp_object_handler, version)
 	end
 
 	#----------------------------------------------------#
 	# append 'size' bytes of memory, using a string to
 	# fill it if necessary.
 	def append(size,str = nil)
-		if str == nil
-			str = "\0"*size
+		if @initialized
+			if str == nil
+				str = "\0"*size
+			end
+			if BLOBSEER._append_(@blob_entry,size,str)
+				return true
+			else
+				return false
+			end
+		else
+			raise "blob must be initialized."
 		end
-		return BLOBSEER._append_(@cpp_object_handler,size,str)
 	end
 end
