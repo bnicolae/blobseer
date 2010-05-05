fabf53a7d4e16fbf2a82267291a00f43a8fc4890
bnicolae <bnicolae@43460812-3f57-0410-813e-9589cf9fb6d1>
2010-05-05 01:41:12 +0000

    added low-level FUSE API skeleton: can list blobs & versions; simple inode management
    git-svn-id: svn+ssh://bnicolae@scm.gforge.inria.fr/svn/blobseer/trunk@279 43460812-3f57-0410-813e-9589cf9fb6d1

---
+++ bindings/blob-fuse/main.cpp
@@ -0,0 +1,102 @@
+/*
+  FUSE BlobSeer local caching support. Main program.
+*/
+
+#define FUSE_USE_VERSION 26
+
+#include <fuse_lowlevel.h>
+
+#include "low_level_ops.hpp"
+
+#define __DEBUG
+#include "common/debug.hpp"
+
+using namespace std;
+
+struct blob_params {
+    char *cfg_file;
+    bool is_help;
+};
+
+#define BLOB_OPT(t, p) { t, offsetof(struct blob_params, p), 1 }
+
+static const struct fuse_opt blob_opts[] = {
+    BLOB_OPT("-C %s", cfg_file),
+    BLOB_OPT("--cfgfile=%s", cfg_file),
+    FUSE_OPT_KEY("-h", 0),
+    FUSE_OPT_KEY("--help", 0),
+    { NULL, 0, 0}
+};
+
+static const char *usage =
+"usage: blob-fuse mountpoint [options]\n"
+"\n"
+"BlobSeer options:\n"
+"    --help|-h                                 print this help message\n"
+"    --cfgfile=<file_name>|-C <file_name>      configuration file (mandatory)\n"
+"\n";
+
+static int blob_process_arg(void *data, const char */*arg*/, int key,
+			      fuse_args *outargs) {
+    blob_params *param = (blob_params *)data;
+    
+    switch (key) {
+    case 0:
+	param->is_help = true;
+	cerr << usage;
+	return fuse_opt_add_arg(outargs, "-ho");
+    default:
+	return 1;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    fuse_args args = FUSE_ARGS_INIT(argc, argv);
+    fuse_chan *ch;
+    blob_params param = { NULL, false };
+
+    char *mountpoint = NULL;
+    int err = -1;
+
+    if (fuse_opt_parse(&args, &param, blob_opts, blob_process_arg) == 0 &&
+	!param.is_help) {
+	if (param.cfg_file == NULL)
+	    cerr << "BlobSeer configuration file is missing." << endl 
+		 << "Use -h or --help to display usage." << endl;
+	else
+	    blob_init(param.cfg_file);
+    }
+
+    if ((param.is_help || param.cfg_file != NULL) &&
+	fuse_parse_cmdline(&args, &mountpoint, NULL, NULL) != -1 &&
+	(ch = fuse_mount(mountpoint, &args)) != NULL) {
+
+	fuse_lowlevel_ops blob_ll_oper;
+	memset(&blob_ll_oper, 0, sizeof(blob_ll_oper));
+	blob_ll_oper.lookup = blob_ll_lookup;
+	blob_ll_oper.getattr = blob_ll_getattr;
+	blob_ll_oper.readdir = blob_ll_readdir;
+	//blob_ll_oper.open = blob_ll_open;
+	//blob_ll_oper.read = blob_ll_read;
+
+	fuse_session *se;
+	se = fuse_lowlevel_new(&args, &blob_ll_oper,
+			       sizeof(blob_ll_oper), NULL);
+	if (se != NULL) {
+	    if (fuse_set_signal_handlers(se) != -1) {
+		fuse_session_add_chan(se, ch);
+		DBG("starting FUSE session loop...");
+		err = fuse_session_loop(se);
+		DBG("FUSE session loop terminated with error code: " << err);		
+		fuse_remove_signal_handlers(se);
+		fuse_session_remove_chan(ch);
+	    }
+	    fuse_session_destroy(se);
+	}
+	fuse_unmount(mountpoint, ch);
+    }
+    fuse_opt_free_args(&args);
+    blob_destroy();
+
+    return err ? 1 : 0;
+}
---
+++ bindings/blob-fuse/object_pool.hpp
@@ -0,0 +1,61 @@
+#ifndef OBJECT_POOL
+#define OBJECT_POOL
+
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/unordered_map.hpp>
+
+template <class Object>
+class object_pool_t {
+private:
+    static const unsigned int DEFAULT_POOL_SIZE = 128;
+
+public:
+    typedef Object object_t;
+    typedef boost::shared_ptr<object_t> pobject_t;
+
+    object_pool_t(boost::function<pobject_t()> gen,
+		  unsigned int pool_size = DEFAULT_POOL_SIZE);
+    pobject_t acquire();
+    void release(pobject_t object);
+
+private:
+    boost::mutex map_lock;
+    typedef std::pair<pobject_t, bool> map_entry_t;
+    typedef boost::unordered_map<pobject_t, bool> object_map_t;
+    object_map_t object_map;
+};
+
+template <class Object>
+object_pool_t<Object>::object_pool_t(
+    boost::function<pobject_t()> gen,
+    unsigned int pool_size) {
+    for (unsigned int i = 0; i < pool_size; i++)
+	object_map.insert(map_entry_t(gen(), false));
+}
+
+template <class Object>
+typename object_pool_t<Object>::pobject_t object_pool_t<Object>::acquire() {
+    boost::mutex::scoped_lock lock(map_lock);
+
+    typename object_map_t::iterator i = object_map.begin();
+    while (i != object_map.end() && i->second)
+	i++;
+    if (i != object_map.end())
+	return i->first;
+    else
+	return pobject_t();
+}
+
+template <class Object>
+void object_pool_t<Object>::release(pobject_t object) {
+    boost::mutex::scoped_lock lock(map_lock);
+
+    typename object_map_t::iterator i = object_map.find(object);
+    if (i != object_map.end())
+	i->second = false;
+    else
+	throw std::runtime_error("object_pool_t::release(): object is not in the pool");
+}
+
+#endif
