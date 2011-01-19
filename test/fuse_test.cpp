e3c92a6a98ac47da224200f93bd3d9bbcb3e8d89
bnicolae <bnicolae@43460812-3f57-0410-813e-9589cf9fb6d1>
2011-01-19 17:18:03 +0000

    use test/fuse_test to initiate CLONE_AND_COMMIT through the FUSE ioctl interface
    git-svn-id: svn://scm.gforge.inria.fr/svn/blobseer/trunk@408 43460812-3f57-0410-813e-9589cf9fb6d1

--- test/fuse_test.cpp
+++ test/fuse_test.cpp
@@ -22,14 +22,6 @@ int main(int argc, char **argv) {
     } else
 	cout << "OK!" << endl;
 
-    cout << "Writing file...";
-    char buffer[] = "ababababab";
-    if (lseek(fd, 10, SEEK_SET) != 10 || write(fd, buffer, 10) != 10) {
-	cout << "FAILED!" << endl;
-	return 2;
-    } else
-	cout << "OK!" << endl;
-
     boost::uint64_t args;
     cout << "CLONE_AND_COMMIT..." << ioctl(fd, CLONE_AND_COMMIT, &args) << endl;
     perror("Latest error: ");
